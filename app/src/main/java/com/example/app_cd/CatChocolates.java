a7e38978f18124232c1af990d306349ae6ecb93b
zaydaMS <i1415646@continental.edu.pe>
2019-12-12 13:06:02 -0500

    catchocolate

---
+++ app/src/main/java/com/example/app_cd/CatChocolates.java
@@ -0,0 +1,14 @@
+package com.example.app_cd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatChocolates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_chocolates);
+    }
+}
