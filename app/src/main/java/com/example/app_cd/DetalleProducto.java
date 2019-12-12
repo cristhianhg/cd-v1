9a1e158ff16eab67cbdeb276d7bac977127ea0ae
cristhianhg <i1415646@continental.edu.pe>
2019-12-12 00:00:33 -0500

    dev.merge2

--- app/src/main/java/com/example/app_cd/DetalleProducto.java
+++ app/src/main/java/com/example/app_cd/DetalleProducto.java
@@ -1,5 +1,13 @@
 package com.example.app_cd;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class DetalleProducto {
+public class DetalleProducto extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_producto);
+    }
 
 }
